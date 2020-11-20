12b7e4f1ec6a2142d8a6d8e88bbaba0d38dfd18e
JavaCommons Technologies <javacommons@gmail.com>
2020-11-20 14:40:28 +0900

    .

---
+++ racket/th07.rkt
@@ -0,0 +1,30 @@
+#lang racket
+; https://docs.racket-lang.org/guide/concurrency.html
+(define result-channel (make-channel))
+(define result-thread
+        (thread (lambda ()
+                  (let loop ()
+                    (display (channel-get result-channel))
+                    (loop)))))
+ 
+(define work-channel (make-channel))
+(define (make-worker thread-id)
+  (thread
+   (lambda ()
+     (let loop ()
+       (define item (channel-get work-channel))
+       (case item
+         [(DONE)
+          (channel-put result-channel
+                       (format "Thread ~a done\n" thread-id))]
+         [else
+          (channel-put result-channel
+                       (format "Thread ~a processed ~a\n"
+                               thread-id
+                               item))
+          (loop)])))))
+(define work-threads (map make-worker '(1 2)))
+(for ([item '(A B C D E F G H DONE DONE)])
+  (channel-put work-channel item))
+(for-each thread-wait work-threads)
+(channel-put result-channel "") ; waits until result-thread has printed all other output
