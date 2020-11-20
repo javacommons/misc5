d9d104fd46ed93fc5ce07e770051c2b1494b1b2b
Your Name <you@example.com>
2020-11-20 14:20:31 +0900

    .

---
+++ racket/th05.rkt
@@ -0,0 +1,12 @@
+#lang racket
+; https://docs.racket-lang.org/guide/concurrency.html
+(define output-semaphore (make-semaphore 1))
+(define (make-thread name)
+  (thread (lambda ()
+            (for [(i 10)]
+              (semaphore-wait output-semaphore)
+              (printf "thread ~a: ~a~n" name i)
+              (semaphore-post output-semaphore)))))
+(define threads
+  (map make-thread '(A B C)))
+(for-each thread-wait threads)
---
+++ racket/th06.rkt
@@ -0,0 +1,13 @@
+#lang racket
+; https://docs.racket-lang.org/guide/concurrency.html
+(define output-semaphore (make-semaphore 1))
+(define (make-thread name)
+  (thread (lambda ()
+            (for [(i 10)]
+              (call-with-semaphore
+               output-semaphore
+               (lambda ()
+                (printf "thread ~a: ~a~n" name i)))))))
+(define threads
+  (map make-thread '(A B C)))
+(for-each thread-wait threads)
