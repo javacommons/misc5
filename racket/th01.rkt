3c88113040c7bc84faf28be942eb15ebfed837d9
Your Name <you@example.com>
2020-11-20 11:14:04 +0900

    .

---
+++ racket/th01.rkt
@@ -0,0 +1,9 @@
+#lang racket
+; https://docs.racket-lang.org/guide/concurrency.html
+(define worker (thread (lambda ()
+                         (let loop ()
+                           (displayln "Working...")
+                           (sleep 0.2)
+                           (loop)))))
+(sleep 2.5)
+(kill-thread worker)
