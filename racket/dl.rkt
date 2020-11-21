3bcfab4dbf844faef28eab41177fc66cd913744c
JavaCommons Technologies <javacommons@gmail.com>
2020-11-22 00:20:43 +0900

    .

--- racket/dl.rkt
+++ racket/dl.rkt
@@ -1,9 +1,10 @@
+#! racket32
 #lang racket
 
 (require "http-request.rkt")
 
 (http-request "https://www.google.com/")
 
-(print 'before)
+(println 'before)
 (define archive (http-request "https://repo.msys2.org/distrib/x86_64/msys2-base-x86_64-20200903.tar.xz"))
-(print 'after)
+(println 'after)
