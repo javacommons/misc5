b7c54f79213db909f5b8d3d31b04ed449204c1c4
JavaCommons Technologies <javacommons@gmail.com>
2020-11-21 09:41:57 +0900

    .

--- racket/gui-01.rkt
+++ racket/gui-01.rkt
@@ -1,24 +1,7 @@
+#! racket32
 #lang racket
 
 (require racket/gui)
-
-
-;(require "call-api.rkt")
-
-(printf "Given arguments: ~s\n"
-        (current-command-line-arguments))
-
-(define api-input (hash "a" 11.11 "b" 22.22))
-api-input
-
-#|
-(call-api "api1" api-input)
-(println "bofore sum")
-(call-api "sum" '[11 99 33])
-(println "after sum")
-(call-api* "sum" 1 2 33)
-(call-api* "none" 1 "abc" 33)
-|#
 
 (find-system-path 'run-file)
 
