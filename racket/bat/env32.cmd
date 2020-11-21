171d5e4133e8c29d67e7dd4fe5b8fc5b7bbed0a2
JavaCommons Technologies <javacommons@gmail.com>
2020-11-21 19:51:32 +0900

    .

---
+++ racket/bat/env32.cmd
@@ -0,0 +1,30 @@
+@echo off
+call :subroutine
+set PATH=%~dp0mingw32\bin;%~dp0usr\local\bin;%~dp0usr\bin;%PATH%
+set MINGW_CHOST=i686-w64-mingw32
+set MINGW_PACKAGE_PREFIX=mingw-w64-i686
+set MINGW_PREFIX=/mingw32
+set MSYSTEM_CARCH=i686
+set MSYSTEM_CHOST=i686-w64-mingw32
+set MSYSTEM_PREFIX=/mingw32
+set MSYSTEM=MINGW32
+IF not exist ~dp0home\%USERNAME% mkdir ~dp0home\%USERNAME%
+goto :eof
+
+:subroutine
+setlocal enabledelayedexpansion
+set NEWPATH=
+for %%A in ("%PATH:;=";"%") do (
+  if /i "%%~A" neq "%~dp0mingw32\bin" (
+    if /i "%%~A" neq "%~dp0mingw64\bin" (
+      if /i "%%~A" neq "%~dp0usr\local\bin" (
+        if /i "%%~A" neq "%~dp0usr\bin" (
+          rem echo %%~A
+          if "!NEWPATH!" equ "" (set NEWPATH=%%~A) else (set NEWPATH=!NEWPATH!;%%~A)
+        )
+      )
+    )
+  )
+)
+endlocal & set PATH=%NEWPATH%
+exit /b
---
+++ racket/bat/env64.cmd
@@ -0,0 +1,30 @@
+@echo off
+call :subroutine
+set PATH=%~dp0mingw64\bin;%~dp0usr\local\bin;%~dp0usr\bin;%PATH%
+set MINGW_CHOST=x86_64-w64-mingw32
+set MINGW_PACKAGE_PREFIX=mingw-w64-x86_64
+set MINGW_PREFIX=/mingw64
+set MSYSTEM_CARCH=x86_64
+set MSYSTEM_CHOST=x86_64-w64-mingw32
+set MSYSTEM_PREFIX=/mingw64
+set MSYSTEM=MINGW64
+IF not exist ~dp0home\%USERNAME% mkdir ~dp0home\%USERNAME%
+goto :eof
+
+:subroutine
+setlocal enabledelayedexpansion
+set NEWPATH=
+for %%A in ("%PATH:;=";"%") do (
+  if /i "%%~A" neq "%~dp0mingw32\bin" (
+    if /i "%%~A" neq "%~dp0mingw64\bin" (
+      if /i "%%~A" neq "%~dp0usr\local\bin" (
+        if /i "%%~A" neq "%~dp0usr\bin" (
+          rem echo %%~A
+          if "!NEWPATH!" equ "" (set NEWPATH=%%~A) else (set NEWPATH=!NEWPATH!;%%~A)
+        )
+      )
+    )
+  )
+)
+endlocal & set PATH=%NEWPATH%
+exit /b
