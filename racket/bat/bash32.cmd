82e19e061814ef97751b7d5c872de3e8d9a73cbb
JavaCommons Technologies <javacommons@gmail.com>
2020-11-21 20:13:47 +0900

    .

--- racket/bat/bash32.cmd
+++ racket/bat/bash32.cmd
@@ -1,8 +1,21 @@
 @echo OFF
-setlocal
-set SCRIPT_CURRENT_DIR=%~dp0
-set PATH=%SCRIPT_CURRENT_DIR%\mingw32\bin;%SCRIPT_CURRENT_DIR%\usr\local\bin;%SCRIPT_CURRENT_DIR%\usr\bin;%PATH%
-set HOME=%SCRIPT_CURRENT_DIR%\home\%USERNAME%
+setlocal enabledelayedexpansion
+set NEWPATH=
+for %%A in ("%PATH:;=";"%") do (
+  if /i "%%~A" neq "%~dp0mingw32\bin" (
+    if /i "%%~A" neq "%~dp0mingw64\bin" (
+      if /i "%%~A" neq "%~dp0usr\local\bin" (
+        if /i "%%~A" neq "%~dp0usr\bin" (
+          rem echo %%~A
+          if "!NEWPATH!" equ "" (set NEWPATH=%%~A) else (set NEWPATH=!NEWPATH!;%%~A)
+        )
+      )
+    )
+  )
+)
+set PATH=%~dp0mingw32\bin;%~dp0usr\local\bin;%~dp0usr\bin;%NEWPATH%
+set NEWPATH=
+set HOME=%~dp0home\%USERNAME%
 IF NOT EXIST %HOME% mkdir %HOME%
 set MINGW_CHOST=i686-w64-mingw32
 set MINGW_PACKAGE_PREFIX=mingw-w64-i686
@@ -11,5 +24,5 @@ set MSYSTEM_CARCH=i686
 set MSYSTEM_CHOST=i686-w64-mingw32
 set MSYSTEM_PREFIX=/mingw32
 set MSYSTEM=MINGW32
-%SCRIPT_CURRENT_DIR%\usr\bin\bash.exe
+%~dp0usr\bin\bash.exe
 endlocal
--- racket/bat/bash64.cmd
+++ racket/bat/bash64.cmd
@@ -1,8 +1,21 @@
 @echo OFF
-setlocal
-set SCRIPT_CURRENT_DIR=%~dp0
-set PATH=%SCRIPT_CURRENT_DIR%\mingw64\bin;%SCRIPT_CURRENT_DIR%\usr\local\bin;%SCRIPT_CURRENT_DIR%\usr\bin;%PATH%
-set HOME=%SCRIPT_CURRENT_DIR%\home\%USERNAME%
+setlocal enabledelayedexpansion
+set NEWPATH=
+for %%A in ("%PATH:;=";"%") do (
+  if /i "%%~A" neq "%~dp0mingw32\bin" (
+    if /i "%%~A" neq "%~dp0mingw64\bin" (
+      if /i "%%~A" neq "%~dp0usr\local\bin" (
+        if /i "%%~A" neq "%~dp0usr\bin" (
+          rem echo %%~A
+          if "!NEWPATH!" equ "" (set NEWPATH=%%~A) else (set NEWPATH=!NEWPATH!;%%~A)
+        )
+      )
+    )
+  )
+)
+set PATH=%~dp0mingw64\bin;%~dp0usr\local\bin;%~dp0usr\bin;%NEWPATH%
+set NEWPATH=
+set HOME=%~dp0home\%USERNAME%
 IF NOT EXIST %HOME% mkdir %HOME%
 set MINGW_CHOST=x86_64-w64-mingw32
 set MINGW_PACKAGE_PREFIX=mingw-w64-x86_64
@@ -11,5 +24,5 @@ set MSYSTEM_CARCH=x86_64
 set MSYSTEM_CHOST=x86_64-w64-mingw32
 set MSYSTEM_PREFIX=/mingw64
 set MSYSTEM=MINGW64
-%SCRIPT_CURRENT_DIR%\usr\bin\bash.exe
+%~dp0usr\bin\bash.exe
 endlocal
