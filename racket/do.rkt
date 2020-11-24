6f424ea9a761ed23fbad73e33862e862aeb43fbd
JavaCommons Technologies <javacommons@gmail.com>
2020-11-24 10:10:27 +0900

    .

--- racket/do.rkt
+++ racket/do.rkt
@@ -14,3 +14,21 @@
   (set! $e (car $l))
   (println $e)
   )
+
+(require compatibility/defmacro)
+
+
+(define-macro (push %x %l)
+  `(let ([%new (cons ,%x ,%l)])
+     (set! ,%l %new)
+     %new))
+ 
+(define-macro (pop %l)
+  `(let ([%car (car ,%l)])
+     (set! ,%l (cdr ,%l))
+     %car))
+ 
+(pop $list)
+$list
+(push 100 $list)
+$list
