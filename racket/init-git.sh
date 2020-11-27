6e6589111056b354fe39412a35814b0db54f5902
JavaCommons Technologies <javacommons@gmail.com>
2020-11-27 09:36:51 +0900

    .

--- racket/init-git.sh
+++ racket/init-git.sh
@@ -2,4 +2,4 @@
 git config --global user.name "JavaCommons Technologies"
 git config --global user.email "javacommons@gmail.com"
 git config --global credential.helper store
-git config pull.rebase false
+git config --global pull.rebase false
