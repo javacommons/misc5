d164c47e9a56b0a27c88ae5b4c154b0a0eb5c22e
JavaCommons Technologies <javacommons@gmail.com>
2020-12-03 01:41:34 +0900

    .

--- racket/oid2.rkt
+++ racket/oid2.rkt
@@ -1,20 +1,26 @@
+#! racket
 #lang racket
 ; https://stackoverflow.com/questions/56386674/unique-identifier-for-racket-objects
 
-(define ht (make-weak-hasheq))
-(define next 0)
+(define $oid-ht (make-weak-hasheq))
+(define $oid-next 0)
+(define $oid-semaphore (make-semaphore 1))
 
-(define (get-id x)
-  (define id (hash-ref ht x #f))
-  (or id
-      (begin0
-        next
-        (hash-set! ht x next)
-        (set! next (+ next 1)))))
+(define (get-oid x)
+  (call-with-semaphore $oid-semaphore
+   (lambda ()
+     (define id (hash-ref $oid-ht x #f))
+     (or id
+         (begin0
+           $oid-next
+           (hash-set! $oid-ht x $oid-next)
+           (set! $oid-next (+ $oid-next 1)))))))
 
-(get-id 'a)
-(get-id 'b)
-(get-id 'a)
+(get-oid 'a)
+(get-oid 'b)
+(get-oid 'a)
 (define $list (list 11 22 33))
-(get-id $list)
-(get-id $list)
+(define $list2 (list 11 22 33))
+(get-oid $list)
+(get-oid $list2)
+(get-oid $list)
