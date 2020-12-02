838d49541b96176d8df7f0cb76c9e21c3582a948
JavaCommons Technologies <javacommons@gmail.com>
2020-12-03 02:08:01 +0900

    .

--- racket/oid2.rkt
+++ racket/oid2.rkt
@@ -6,21 +6,41 @@
 (define $oid-next 0)
 (define $oid-semaphore (make-semaphore 1))
 
-(define (get-oid x)
-  (call-with-semaphore $oid-semaphore
+(define (to-oid %x)
+  (call-with-semaphore
+   $oid-semaphore
    (lambda ()
-     (define id (hash-ref $oid-ht x #f))
-     (or id
+     (define %oid (hash-ref $oid-ht %x #f))
+     (or %oid
          (begin0
            $oid-next
-           (hash-set! $oid-ht x $oid-next)
+           (hash-set! $oid-ht %x $oid-next)
            (set! $oid-next (+ $oid-next 1)))))))
 
-(get-oid 'a)
-(get-oid 'b)
-(get-oid 'a)
+(define (from-oid %oid)
+  (define $result (void))
+  (hash-for-each
+   $oid-ht
+   (lambda (%key %value)
+     (when (= %value %oid)
+       (set! $result %key))))
+  $result)
+
+(to-oid 'a)
+(to-oid 'b)
+(to-oid 'a)
 (define $list (list 11 22 33))
 (define $list2 (list 11 22 33))
-(get-oid $list)
-(get-oid $list2)
-(get-oid $list)
+(to-oid $list)
+(to-oid $list2)
+(to-oid $list)
+
+(from-oid 2)
+
+(to-oid (void))
+
+(hash-count $oid-ht)
+(set! $list (void))
+(set! $list2 (void))
+(collect-garbage)
+(hash-count $oid-ht)
