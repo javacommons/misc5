fd409f27471c7b2a44f78e48ed718b38f99e9610
JavaCommons Technologies <javacommons@gmail.com>
2020-12-02 23:00:14 +0900

    .

--- racket/zip-install.rkt
+++ racket/zip-install.rkt
@@ -28,73 +28,70 @@
 
 (define (run-file-dir)
   (let*-values
-   ([(%run-file)
-     (simplify-path (path->complete-path (find-system-path 'run-file)))]
-    [(%base %name %must-be-dir?) (split-path %run-file)])
-   %base))
+      ([(%run-file)
+        (simplify-path (path->complete-path (find-system-path 'run-file)))]
+       [(%base %name %must-be-dir?) (split-path %run-file)])
+    %base))
 
 (define prepare-dir%
-  (class
-   object%
-   (init :root-dir :zip-base)
-   (super-new)
-   (println :root-dir)
-   (define $software-dir (build-path :root-dir ".software"))
-   (define $target-dir (build-path $software-dir :zip-base))
-   (println $target-dir)
-   (define $run-file-dir (run-file-dir))
-   (define $zip-path (format "~a\\~a.zip" $run-file-dir :zip-base))
-   (define $file-in (open-input-file $zip-path #:mode 'binary))
-   (println $file-in)
-   (cond
-    [(not (directory-exists? $target-dir))
-     (make-directory* $target-dir)
-     (current-directory $target-dir)
-     (current-directory)
-     (println "unzip start")
-     (unzip $file-in)
-     (println "unzip end")])
-   (define/public (:get-target-dir)
-     $target-dir)
-   )
+  (class object%
+    (init :root-dir :zip-base) (super-new)
+    (println :root-dir)
+    (define &software-dir (build-path :root-dir ".software"))
+    (define &target-dir (build-path &software-dir :zip-base))
+    (println &target-dir)
+    (define &run-file-dir (run-file-dir))
+    (define &zip-path (format "~a\\~a.zip" &run-file-dir :zip-base))
+    (define &file-in (open-input-file &zip-path #:mode 'binary))
+    (println &file-in)
+    (cond
+      [(not (directory-exists? &target-dir))
+       (make-directory* &target-dir)
+       (current-directory &target-dir)
+       (current-directory)
+       (println "unzip start")
+       (unzip &file-in)
+       (println "unzip end")])
+    (define/public (:get-target-dir)
+      &target-dir)
+    )
   )
 
 (define set-path%
-  (class
-   object% (super-new)
-   (init :member)
-   (define $member (if (path? :member) (path->string :member) :member))
-   (println $member)
-   (set! $member (string-replace $member "/" "\\"))
-   (println $member)
-   (define $path (get-registry-env "USER" "PATH"))
-   (println $path)
-   (define $path-list (string-split $path ";"))
-   (println $path-list)
-   (set! $path-list (cons $member $path-list))
-   (set! $path-list (remove-duplicates $path-list equal?))
-   (println $path-list)
-   (set! $path (string-join $path-list ";"))
-   (println $path)
-   (set-registry-env! "USER" "PATH" $path)
-   )
+  (class object%
+    (init :member) (super-new)
+    (define &member (if (path? :member) (path->string :member) :member))
+    (println &member)
+    (set! &member (string-replace &member "/" "\\"))
+    (println &member)
+    (define &path (get-registry-env "USER" "PATH"))
+    (println &path)
+    (define &path-list (string-split &path ";"))
+    (println &path-list)
+    (set! &path-list (cons &member &path-list))
+    (set! &path-list (remove-duplicates &path-list equal?))
+    (println &path-list)
+    (set! &path (string-join &path-list ";"))
+    (println &path)
+    (set-registry-env! "USER" "PATH" &path)
+    )
   )
 
 (define create-simple-shortcut%
   (class object% (super-new)
     (init :shortcut-path :target-path)
-    (define $wshell (com-create-instance "WScript.Shell" 'local))
-    (define $sc (com-invoke $wshell "CreateShortcut" :shortcut-path))
-    (com-set-property! $sc "TargetPath" :target-path)
-    (com-invoke $sc "Save")
+    (define &wshell (com-create-instance "WScript.Shell" 'local))
+    (define &sc (com-invoke &wshell "CreateShortcut" :shortcut-path))
+    (com-set-property! &sc "TargetPath" :target-path)
+    (com-invoke &sc "Save")
     )
   )
 
 (provide
-  get-registry-env
-  set-registry-env!
-  delete-registry-env!
-  run-file-dir
-  prepare-dir%
-  set-path%
-  create-simple-shortcut%)
+ get-registry-env
+ set-registry-env!
+ delete-registry-env!
+ run-file-dir
+ prepare-dir%
+ set-path%
+ create-simple-shortcut%)
