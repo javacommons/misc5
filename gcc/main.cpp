871350689eafa46f41aa87bff5bcc34f2ff7c8ec
JavaCommons Technologies <javacommons@gmail.com>
2020-12-04 04:09:21 +0900

    .

---
+++ gcc/main.cpp
@@ -0,0 +1,45 @@
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+#include <bits/stdc++.h>
+using namespace std;
+
+std::ostream &operator<<(std::ostream &dest, __int128_t value) {
+  std::ostream::sentry s(dest);
+  if (s) {
+    __uint128_t tmp = value < 0 ? -value : value;
+    char buffer[128];
+    char *d = std::end(buffer);
+    do {
+      --d;
+      *d = "0123456789"[tmp % 10];
+      tmp /= 10;
+    } while (tmp != 0);
+    if (value < 0) {
+      --d;
+      *d = '-';
+    }
+    int len = std::end(buffer) - d;
+    if (dest.rdbuf()->sputn(d, len) != len) {
+      dest.setstate(std::ios_base::badbit);
+    }
+  }
+  return dest;
+}
+
+__int128 parse(string &s) {
+  __int128 ret = 0;
+  for (int i = 0; i < s.length(); i++)
+    if ('0' <= s[i] && s[i] <= '9')
+      ret = 10 * ret + s[i] - '0';
+  return ret;
+}
+
+int main() {
+  string s = "187821878218782187821878218782";
+  __int128 x = parse(s);
+  x *= 2;
+  cout << x << endl;
+  //cout << std::to_string(x) << endl;
+  json j = x;
+  cout << j.dump() << endl;
+}
