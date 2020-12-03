c67d7689f4d76dca674427f59a4ca306319c3296
JavaCommons Technologies <javacommons@gmail.com>
2020-12-03 19:53:53 +0900

    .

--- mylib/mylib-oid.rkt
+++ mylib/mylib-oid.rkt
@@ -1,21 +1,21 @@
 #lang racket
 (provide to-oid from-oid oid-count)
 
-(define $oid-ht (make-weak-hasheq))
-(define $oid-reverse-ht (make-hasheq))
+(define $oid-ht-1 (make-weak-hasheq))
+(define $oid-ht-2 (make-hasheq))
 (define $oid-next 0)
 (define $oid-semaphore (make-semaphore 1))
 
-(define (to-oid %x)
+(define (to-oid %v)
   (call-with-semaphore
    $oid-semaphore
    (lambda ()
-     (define %oid (hash-ref $oid-ht %x #f))
+     (define %oid (hash-ref $oid-ht-1 %v #f))
      (or %oid
          (begin0
            $oid-next
-           (hash-set! $oid-ht %x $oid-next)
-           (hash-set! $oid-reverse-ht $oid-next (make-weak-box %x))
+           (hash-set! $oid-ht-1 %v $oid-next)
+           (hash-set! $oid-ht-2 $oid-next (make-weak-box %v))
            (set! $oid-next (+ $oid-next 1)))))))
 
 (void (to-oid #f))
@@ -24,9 +24,9 @@
   (call-with-semaphore
    $oid-semaphore
    (lambda ()
-     (define %box (hash-ref $oid-reverse-ht %oid #f))
+     (define %box (hash-ref $oid-ht-2 %oid #f))
      (and %box
           (weak-box-value %box)))))
 
 (define (oid-count)
-  (hash-count $oid-ht))
+  (hash-count $oid-ht-1))
