ea36b991d549d49f420ef2e1231ff5bd8e262735
JavaCommons Technologies <javacommons@gmail.com>
2020-12-04 11:58:22 +0900

    .

--- inst-qt.sh
+++ inst-qt.sh
@@ -3,6 +3,7 @@ PACKAGE_PREFIX="${MINGW_PACKAGE_PREFIX:-mingw-w64-x86_64}"
 echo $PACKAGE_PREFIX
 pacman -S --needed --noconfirm \
 	autoconf \
+	automake-wrapper \
 	make \
 	libtool \
 	$PACKAGE_PREFIX-toolchain \
