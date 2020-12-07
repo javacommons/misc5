a3dcba691ac3fbd6ccf0e12fe99046e7eeefdbff
JavaCommons Technologies <javacommons@gmail.com>
2020-12-07 23:33:23 +0900

    .

--- racket/app01/libarchive.cpp
+++ racket/app01/libarchive.cpp
@@ -9,6 +9,7 @@
 #include <archive_entry.h>
 #include <sys/utime.h>
 #include "strconv.h"
+#include "strutil.h"
 #include "vardecl.h"
 #include "apiv1.hpp"
 
@@ -16,44 +17,6 @@ using namespace std;
 
 LibArchive::LibArchive()
 {
-}
-
-inline bool ends_with(std::string const & value, std::string const & ending)
-{
-    if (ending.size() > value.size()) return false;
-    return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
-}
-inline bool ends_with(std::wstring const & value, std::wstring const & ending)
-{
-    if (ending.size() > value.size()) return false;
-    return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
-}
-
-std::string replaceAll(std::string &replacedStr, std::string from, std::string to) // https://www.sejuku.net/blog/54493
-{
-    unsigned int pos = replacedStr.find(from);
-    int toLen = to.length();
-    if (from.empty()) {
-        return replacedStr;
-    }
-    while ((pos = replacedStr.find(from, pos)) != std::string::npos) {
-        replacedStr.replace(pos, from.length(), to);
-        pos += toLen;
-    }
-    return replacedStr;
-}
-std::wstring replaceAll(std::wstring &replacedStr, std::wstring from, std::wstring to) // https://www.sejuku.net/blog/54493
-{
-    unsigned int pos = replacedStr.find(from);
-    int toLen = to.length();
-    if (from.empty()) {
-        return replacedStr;
-    }
-    while ((pos = replacedStr.find(from, pos)) != std::string::npos) {
-        replacedStr.replace(pos, from.length(), to);
-        pos += toLen;
-    }
-    return replacedStr;
 }
 
 static std::map<void *, json> handle_map;
@@ -82,7 +45,7 @@ std::string to_native_path(const std::string &s)
 {
     std::filesystem::path path = utf8_to_wide(s);
     auto native = path.native();
-    replaceAll(native, L"\\", L"/");
+    replace_all(native, L"\\", L"/");
     native = std::regex_replace(native, std::wregex(L"/+$"), L"");
     return wide_to_utf8(native);
 }
@@ -199,71 +162,3 @@ json api_archive_entry_write(const json &args)
     }
     return copy;
 }
-
-
-#if 0x0
-bool extract_archive(const std::wstring &archive_path, const std::wstring &output_path) {
-    //QDir dir(output_path);
-    //dir.removeRecursively();
-    int r;
-    struct archive *a = archive_read_new();
-#if 0x0
-    archive_read_support_compression_all(a);
-#else
-    archive_read_support_filter_all(a); // https://manpages.debian.org/stretch/libarchive-dev/libarchive_changes.3.en.html
-#endif
-    archive_read_support_format_all(a);
-    if ((r = archive_read_open_filename_w(a, archive_path.c_str(), 10240))) {
-        std::cout << "Could not open:" << wide_to_utf8(archive_path) << std::endl;
-        return false;
-    }
-    std::cout << "Open:" << wide_to_utf8(archive_path) << std::endl;
-    for (;;) {
-        struct archive_entry *entry;
-        r = archive_read_next_header(a, &entry);
-        if (r == ARCHIVE_EOF)
-            break;
-        if (r < ARCHIVE_OK)
-            fprintf(stderr, "%s\n", archive_error_string(a));
-        if (r < ARCHIVE_WARN)
-            return false;
-        std::wstring entry_pathname = archive_entry_pathname_w(entry);
-        //qDebug() << "entry_pathname:" << entry_pathname;
-        la_int64_t entry_size = archive_entry_size(entry);
-        UNUSED_VARIABLE(entry_size);
-        //qDebug() << "entry_size:" << entry_size;
-        time_t mtime = archive_entry_mtime(entry);
-        //QDateTime mtime_dt = QDateTime::fromTime_t(mtime);
-        //qDebug() << "mtime:" << mtime_dt;
-        std::wstring expFilePath = output_path + L"/" + entry_pathname;
-        //qDebug() << "expFilePath:" << expFilePath;
-        if(ends_with(entry_pathname, L"/")) {
-            std::filesystem::create_directories(expFilePath);
-            //QDir dir;
-            //dir.mkpath(expFilePath);
-            continue;
-        }
-        //QFile file(expFilePath);
-        std::filesystem::path file = expFilePath;
-        //QFileInfo finfo(file);
-        //QDir dir = finfo.dir();
-        std::filesystem::path dir = file.parent_path();
-        //qDebug() << dir.absolutePath();
-        //dir.mkpath(dir.absolutePath());
-        std::filesystem::create_directories(dir);
-        FILE *fp;;
-        if((fp = _wfopen(expFilePath.c_str(), L"wb"))) {
-            int fd = fileno(fp);
-            archive_read_data_into_fd(a, fd);
-            fclose(fp);
-            _utimbuf utbuff;
-            utbuff.actime = mtime;
-            utbuff.modtime = mtime;
-            _wutime(expFilePath.c_str(), &utbuff);
-        }
-    }
-    archive_read_close(a);
-    archive_read_free(a);
-    return true;
-}
-#endif
--- racket/strutil.h
+++ racket/strutil.h
@@ -29,4 +29,31 @@ inline bool ends_with(std::wstring const & value, std::wstring const & ending)
     return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
 }
 
+std::string replace_all(std::string &replacedStr, std::string from, std::string to) // https://www.sejuku.net/blog/54493
+{
+    unsigned int pos = replacedStr.find(from);
+    int toLen = to.length();
+    if (from.empty()) {
+        return replacedStr;
+    }
+    while ((pos = replacedStr.find(from, pos)) != std::string::npos) {
+        replacedStr.replace(pos, from.length(), to);
+        pos += toLen;
+    }
+    return replacedStr;
+}
+std::wstring replace_all(std::wstring &replacedStr, std::wstring from, std::wstring to) // https://www.sejuku.net/blog/54493
+{
+    unsigned int pos = replacedStr.find(from);
+    int toLen = to.length();
+    if (from.empty()) {
+        return replacedStr;
+    }
+    while ((pos = replacedStr.find(from, pos)) != std::string::npos) {
+        replacedStr.replace(pos, from.length(), to);
+        pos += toLen;
+    }
+    return replacedStr;
+}
+
 #endif //STRUTIL_H
