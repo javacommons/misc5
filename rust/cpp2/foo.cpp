f98949fe3ce48e5d7abe43902d6cadf5806852b6
JavaCommons Technologies <javacommons@gmail.com>
2020-12-10 22:53:16 +0900

    .

--- rust/cpp2/foo.cpp
+++ rust/cpp2/foo.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+#include "vardecl.h"
 
 extern"C" void foo(){
     cout << "hello c++!" << endl;
+}
+
+// fn my_printer(s: *const c_char);
+extern"C" void my_printer(const char *msg){
+    cout << "msg=" << msg << endl;
+}
+
+// fn my_string() -> *const c_char;
+extern"C" const char *my_string(){
+    TLS_VARIABLE_DECL std::string msg;
+    msg = "<hello>";
+    return msg.c_str();
 }
--- rust/cpp2/main.rs
+++ rust/cpp2/main.rs
@@ -1,8 +1,29 @@
-#[link(name="foo", kind="static")]
-extern{
+use std::ffi::CStr;
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+#[link(name = "foo", kind = "static")]
+extern "C" {
     fn foo();
+    fn my_printer(s: *const c_char);
+    fn my_string() -> *const c_char;
 }
 
 fn main() {
-    unsafe {foo();};
+    unsafe {
+        foo();
+    };
+    // We are certain that our string doesn't have 0 bytes in the middle,
+    // so we can .unwrap()
+    let c_to_print = CString::new("Hello, world!").unwrap();
+    unsafe {
+        my_printer(c_to_print.as_ptr());
+    }
+    unsafe {
+        let slice = CStr::from_ptr(my_string());
+        println!(
+            "string buffer size without nul terminator: {}",
+            slice.to_bytes().len()
+        );
+    }
 }
