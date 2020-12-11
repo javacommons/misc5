690439f7a705623138b972ae48cd5724029f1215
JavaCommons Technologies <javacommons@gmail.com>
2020-12-11 19:21:27 +0900

    .

--- web/both.cpp
+++ web/both.cpp
@@ -1,3 +1,4 @@
+#define CPPHTTPLIB_ZLIB_SUPPORT
 #include "httplib.h"
 #include <iostream>
 #include <thread>
@@ -22,6 +23,45 @@ void worker()
     res.set_content(numbers, "text/plain");
   });
 
+  svr.Post("/content_receiver",
+           [&](const Request &req, Response &res, const ContentReader &content_reader) {
+             cout << "handle: /content_receiver" << endl;
+             if (req.is_multipart_form_data())
+             {
+               cout << "if (req.is_multipart_form_data())" << endl;
+               MultipartFormDataItems files;
+               content_reader(
+                   [&](const MultipartFormData &file) {
+                     files.push_back(file);
+                     return true;
+                   },
+                   [&](const char *data, size_t data_length) {
+                     files.back().content.append(data, data_length);
+                     return true;
+                   });
+               res.set_content("(req.is_multipart_form_data())", "text/plain");
+             }
+             else
+             {
+               cout << "else " << req.get_header_value("Content-Type") << endl;
+               std::string body;
+               content_reader([&](const char *data, size_t data_length) {
+                 body.append(data, data_length);
+                 return true;
+               });
+               res.set_content(body, "text/plain");
+             }
+           });
+
+  svr.Post("/post",
+           [&](const Request &req, Response &res, const ContentReader &content_reader) {
+             cout << "handle: /post" << endl;
+             if (req.has_param("name"))
+             {
+               std::string val = req.get_param_value("name");
+               cout << "handle: /post name=" << val << endl;
+             }
+           });
   svr.listen("127.0.0.1", 1234);
 }
 
@@ -34,6 +74,10 @@ int main(void)
 #endif
 
   httplib::Client cli("127.0.0.1", 1234);
+  cli.set_default_headers({{"Accept-Encoding", "gzip, deflate"}});
+  cli.set_keep_alive(true);
+  cli.set_follow_location(true);
+  cli.set_compress(true);
 
   auto res = cli.Get("/hi");
   if (res)
@@ -52,6 +96,26 @@ int main(void)
       //cout << res->get_header_value("Content-Type") << endl;
       cout << res->body << endl;
     }
+  }
+
+  cout << "/content_receiver" << endl;
+  res = cli.Post("/content_receiver", "abc", "text/plainXXX");
+  if (res)
+  {
+    cout << res->status << endl;
+    //cout << res->get_header_value("Content-Type") << endl;
+    cout << res->body << endl;
+  }
+
+  Params params;
+  params.emplace("name", "john");
+  params.emplace("note", "coder");
+  res = cli.Post("/content_receiver", params);
+  if (res)
+  {
+    cout << res->status << endl;
+    //cout << res->get_header_value("Content-Type") << endl;
+    cout << res->body << endl;
   }
 
   return 0;
--- web/mk.sh
+++ web/mk.sh
@@ -1,4 +1,4 @@
 #! bash -uvx
 #g++ -o server.exe server.cpp -lws2_32
 #g++ -o client.exe client.cpp -lws2_32
-g++ -o both.exe both.cpp -lws2_32 && ./both.exe
+g++ -o both.exe both.cpp -lws2_32 -lz && ./both.exe
