c69c410cd04c4489127b54bc5e248f8c2f360b1e
JavaCommons Technologies <javacommons@gmail.com>
2020-12-15 16:35:06 +0900

    .

--- demo/format.cpp
+++ demo/format.cpp
@@ -4,15 +4,25 @@
 #include "strconv.h"
 
 template <typename... Args>
-static inline std::wstring formatX(const std::wstring& format_str, Args&&... args) {
-  return fmt::format(format_str, args...);
+static inline std::string formatA2(const std::string &format_str, Args &&... args)
+{
+  return utf8_to_ansi(fmt::format(format_str, args...));
 }
 
+template <typename... Args>
+static inline std::string formatA2(const std::wstring &format_str, Args &&... args)
+{
+  return wide_to_ansi(fmt::format(format_str, args...));
+}
 
-int main() {
+int main()
+{
   using namespace std::literals::chrono_literals;
-  fmt::print("Default format: {} {}\n", 42s, 100ms);
-  fmt::print("strftime-like format: {:%H:%M:%S}\n", 3h + 15min + 30s);
-  std::wstring ws = formatX(L"this is(これは) abc {}", 123);
-  std::cout << wide_to_ansi(ws) << std::endl;
+  std::cout << formatA2(u8"Default format: {} {}", 42s, 100ms) << std::endl;
+  std::cout << formatA2(u8"strftime-like format: {:%H:%M:%S}", 3h + 15min + 30s) << std::endl;
+  std::wstring hello = L"ハロー";
+  std::cout << formatA2(L"this is(これは) {}", hello) << std::endl;
+  std::string goodby = u8"グッバイ";
+  std::cout << formatA2(u8"that is(あれは) {}", goodby) << std::endl;
+  return 0;
 }
