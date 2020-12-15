f328fa1dd91050248e98cc7598e7bd2d7017a929
JavaCommons Technologies <javacommons@gmail.com>
2020-12-15 22:36:53 +0900

    .

--- demo/format.cpp
+++ demo/format.cpp
@@ -1,23 +1,11 @@
-#include "strconv2.h"
+#include "format.h"
 #include <iostream>
-
-using strconv::format;
-using strconv::formatA;
-using strconv::printA;
-using strconv::utf8;
 
 int main()
 {
-	char8_t ch = 0;
-	std::string s = utf8(u8"abc");
-	using namespace std::literals::chrono_literals;
-	std::cout << format(utf8(u8"Default format: {} {}"), 42s, 100ms) << std::endl;
-	std::cout << format("strftime-like format: {:%H:%M:%S}", 3h + 15min + 30s) << std::endl;
-	std::wstring hello = L"ハロー";
-	std::cout << formatA(L"this is(これは) {}", hello) << std::endl;
-	printA(L"this is(これは) {}\n", hello);
-	std::string goodby = utf8(u8"グッバイ");
-	std::cout << formatA(utf8(u8"that is(あれは) {}"), goodby) << std::endl;
-	printA(utf8(u8"that is(あれは) {}\n"), goodby);
-	return 0;
+  fmt::print("Hello, {}!\n", "world");  // uses Python-like format string syntax
+  fmt::printf("Hello, %s!\n", "world"); // uses printf format string syntax
+  std::string s = fmt::format("{0}{1}{0}", "abra", "cad");
+  std::cout << s << std::endl;
+  return 0;
 }
