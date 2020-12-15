3905d474815093c2922d286f2009bfcbdc54e3f1
JavaCommons Technologies <javacommons@gmail.com>
2020-12-15 21:48:01 +0900

    .

--- demo/format.cpp
+++ demo/format.cpp
@@ -8,15 +8,16 @@ using strconv::utf8;
 
 int main()
 {
-  std::string s = utf8(u8"abc");
-  using namespace std::literals::chrono_literals;
-  std::cout << format(utf8(u8"Default format: {} {}"), 42s, 100ms) << std::endl;
-  std::cout << format("strftime-like format: {:%H:%M:%S}", 3h + 15min + 30s) << std::endl;
-  std::wstring hello = L"ハロー";
-  std::cout << formatA(L"this is(これは) {}", hello) << std::endl;
-  printA(L"this is(これは) {}\n", hello);
-  std::string goodby = "グッバイ";
-  std::cout << formatA("that is(あれは) {}", goodby) << std::endl;
-  printA("that is(あれは) {}\n", goodby);
-  return 0;
+	char8_t ch = 0;
+	std::string s = utf8(u8"abc");
+	using namespace std::literals::chrono_literals;
+	std::cout << format(utf8(u8"Default format: {} {}"), 42s, 100ms) << std::endl;
+	std::cout << format("strftime-like format: {:%H:%M:%S}", 3h + 15min + 30s) << std::endl;
+	std::wstring hello = L"ハロー";
+	std::cout << formatA(L"this is(これは) {}", hello) << std::endl;
+	printA(L"this is(これは) {}\n", hello);
+	std::string goodby = utf8(u8"グッバイ");
+	std::cout << formatA(utf8(u8"that is(あれは) {}"), goodby) << std::endl;
+	printA(utf8(u8"that is(あれは) {}\n"), goodby);
+	return 0;
 }
