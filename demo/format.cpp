862e8888706dc26ac0f58b16e6d640bf2ce5cc78
JavaCommons Technologies <javacommons@gmail.com>
2020-12-15 15:56:25 +0900

    .

--- demo/format.cpp
+++ demo/format.cpp
@@ -1,8 +1,18 @@
 #define FMT_HEADER_ONLY
 #include <fmt/chrono.h>
+#include <iostream>
+#include "strconv.h"
+
+template <typename... Args>
+static inline std::wstring formatX(const std::wstring& format_str, Args&&... args) {
+  return fmt::format(format_str, args...);
+}
+
 
 int main() {
   using namespace std::literals::chrono_literals;
   fmt::print("Default format: {} {}\n", 42s, 100ms);
   fmt::print("strftime-like format: {:%H:%M:%S}\n", 3h + 15min + 30s);
+  std::wstring ws = formatX(L"this is(これは) abc {}", 123);
+  std::cout << wide_to_ansi(ws) << std::endl;
 }
