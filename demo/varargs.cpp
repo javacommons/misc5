352e290b0506821938c1c3fa2dc311aecdfb1eed
JavaCommons Technologies <javacommons@gmail.com>
2020-12-16 19:56:09 +0900

    .

--- demo/varargs.cpp
+++ demo/varargs.cpp
@@ -1,21 +1,39 @@
+#include <windows.h>
+#include <iostream>
+#include <variant>
+#include <string>
+#include <vector>
+#include <map>
+
+using atom = std::variant<int, char, std::string, void *>;
+using list = std::vector<atom>;
+using dictionary = std::map<std::string, atom>;
+using variable = std::variant<int, char, std::string, void *, list, dictionary>;
+using variableList = std::vector<variable>;
+
 #include <iostream>
 #include <utility>
 
 // パラメータパックが空になったら終了
-void print() {}
+void print(variableList &vars) {
+  std::cout << vars.size() << std::endl;
+}
 
 // ひとつ以上のパラメータを受け取るようにし、
 // 可変引数を先頭とそれ以外に分割する
 template <class Head, class... Tail>
-void print(Head&& head, Tail&&... tail)
+void print(variableList &vars, Head&& head, Tail&&... tail)
 {
   std::cout << head << std::endl;
+  vars.push_back(head);
 
   // パラメータパックtailをさらにheadとtailに分割する
-  print(std::forward<Tail>(tail)...);
+  print(vars, std::forward<Tail>(tail)...);
 }
 
 int main()
 {
-  print(1, 'a', "hello");
+  variableList vars;
+  print(vars, 1, 'a', "hello");
+  std::cout << vars.size() << std::endl;
 }
