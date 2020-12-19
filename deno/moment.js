0920ed8f000cb4af890c119c3d78339430ae6507
JavaCommons Technologies <javacommons@gmail.com>
2020-12-20 01:49:12 +0900

    .

---
+++ deno/moment.js
@@ -0,0 +1,3 @@
+// @deno-types="https://raw.githubusercontent.com/moment/moment/develop/ts3.1-typings/moment.d.ts"
+import moment from "https://raw.githubusercontent.com/moment/moment/develop/dist/moment.js";
+console.log(moment.now());
