ba7b99b27b23d643ce01b58fa907c5903bcb0497
JavaCommons Technologies <javacommons@gmail.com>
2020-12-19 20:08:33 +0900

    .

--- demo/server.cpp
+++ demo/server.cpp
@@ -1,23 +1,21 @@
 #include "json-ipc.h"
-//#include "zmqipc.hpp"
 #include <cstdlib>
-//#include <iostream>
 #include "strconv.h"
 #include "vardecl.h"
 
-using namespace std;
+//using namespace std;
+
+void on_exit()
+{
+    formatA(std::cout, "on_exit\n");
+    ::MessageBoxW(NULL, L"on_exit()", L"server.exe", MB_OK);
+}
 
 json dummy_api(const json &input)
 {
     json output = input;
     output["out"] = 123.45;
     return output;
-}
-
-void on_exit()
-{
-    formatA(std::cout, "on_exit\n");
-    ::MessageBoxW(NULL, L"on_exit()", L"server.exe", MB_OK);
 }
 
 typedef json (*json_api)(const json &input);
@@ -42,9 +40,9 @@ static const char *handler(const char *api, const char *input)
     if(!func) return nullptr;
     json output = func(json::parse(input));
     formatA(std::cout, "handler: output=%s\n", output.dump().c_str());
-    return output.dump().c_str();
+    TLS_VARIABLE std::string ret = output.dump();
+    return ret.c_str();
 }
-
 
 int main(int argc, char *argv[])
 {
@@ -53,30 +51,17 @@ int main(int argc, char *argv[])
 
     formatA(std::cout, "server(1)\n");
 
-#if 0x0
-    std::string endpoint;
-    bool debug;
-    if (!find_endpont_from_args(endpoint, &debug))
-        return 1;
-#else
     int debug;
     const char *endpoint = ipc_find_endpont_from_args(&debug);
     if(!endpoint) return 1;
-#endif
+
     if (debug)
         std::atexit(on_exit);
 
     formatA(std::cout, "server(2)\n");
 
-#if 0x0
-    ZmqIPC ipc;
-    REGISTER_JSON_API(ipc, dummy_api);
-    if (!ipc.open_server(endpoint))
-        return 1;
-#else
     REGISTER_JSON_API(dummy_api);
     json_ipc *ipc = ipc_open_server(endpoint, handler);
-#endif
 
     formatA(std::cout, "server(3)\n");
 
