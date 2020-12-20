d7c3aec5c04657101536e74d9a5a3a18c48ead74
JavaCommons Technologies <javacommons@gmail.com>
2020-12-20 16:45:46 +0900

    .

--- init-git.sh
+++ init-git.sh
@@ -1,5 +1,6 @@
 #! bash -uvx
 git config --global user.name "JavaCommons Technologies"
 git config --global user.email "javacommons@gmail.com"
+git config --global core.quotepath false #日本語ファイル名がエスケープされないように
 git config --global credential.helper store
 git config --global pull.rebase false
