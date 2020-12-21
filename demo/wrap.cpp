1b45b019b07a76cb906cb892b27f9edfd2b14850
JavaCommons Technologies <javacommons@gmail.com>
2020-12-22 06:04:56 +0900

    .

--- demo/wrap.cpp
+++ demo/wrap.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-
-using namespace std;
+#include <iomanip>
 
 class ostreamWrapper
 {
 private:
-    ostream &m_ostrm;
+    std::ostream &m_ostrm;
 
 public:
-    ostreamWrapper(ostream &ostrm) : m_ostrm(ostrm) {}
+    ostreamWrapper(std::ostream &ostrm) : m_ostrm(ostrm) {}
 
     template <typename T>
     ostreamWrapper &operator<<(const T &x)
@@ -42,7 +41,38 @@ public:
 
 int main()
 {
+    using namespace std;
     ostreamWrapper osw(std::cout);
     osw << 1 << " texto " << std::string(" otro texto ") << 1.2345 << std::endl;
+    int x = 123;
+    osw << setw(5) << setiosflags(ios::left) << x << "abc" << endl;
+    double A = 100;
+    double B = 2001.5251;
+    double C = 201455.2646;
+
+    // We can use setbase(16) here instead of hex
+
+    // formatting
+    osw << hex << left << showbase << nouppercase;
+
+    // actual printed part
+    osw << (long long)A << endl;
+
+    // We can use dec here instead of setbase(10)
+
+    // formatting
+    osw << setbase(10) << right << setw(15)
+        << setfill('_') << showpos
+        << fixed << setprecision(2);
+
+    // actual printed part
+    osw << B << endl;
+
+    // formatting
+    osw << scientific << uppercase
+        << noshowpos << setprecision(9);
+
+    // actual printed part
+    osw << C << endl;
     return 0;
 }
