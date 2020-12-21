73999b20a43e3377dffbb567885c2fce79b23e85
JavaCommons Technologies <javacommons@gmail.com>
2020-12-22 04:23:55 +0900

    .

---
+++ demo/nostd.cpp
@@ -0,0 +1,40 @@
+// https://github.com/martinmoene/any-lite/tree/master/example
+// https://github.com/martinmoene/byte-lite/tree/master/example
+// https://github.com/martinmoene/expected-lite/tree/master/example
+// https://github.com/martinmoene/observer-ptr-lite/tree/master/example
+// https://github.com/martinmoene/optional-lite/tree/master/example
+// https://github.com/martinmoene/ring-span-lite/tree/master/example
+// https://github.com/martinmoene/span-lite/tree/master/example
+// https://github.com/martinmoene/status-value-lite/tree/master/example
+// https://github.com/martinmoene/string-view-lite/tree/master/example
+// https://github.com/martinmoene/value-ptr-lite/tree/master/example
+// https://github.com/martinmoene/variant-lite/tree/master/example
+
+#include <nonstd/optional.hpp>
+
+#include <cstdlib>
+#include <iostream>
+
+using nonstd::optional;
+using nonstd::nullopt;
+
+optional<int> to_int( char const * const text )
+{
+    char * pos = NULL;
+    const int value = static_cast<int>( strtol( text, &pos, 0 ) );
+
+    return pos == text ? nullopt : optional<int>( value );
+}
+
+int main( int argc, char * argv[] )
+{
+    const char * text = argc > 1 ? argv[1] : "42";
+
+    optional<int> oi = to_int( text );
+
+    if ( oi ) std::cout << "'" << text << "' is " << *oi << std::endl;
+    else      std::cout << "'" << text << "' isn't a number" << std::endl;
+}
+
+// cl -nologo -W3 -EHsc -I../include to_int.cpp && to_int x1
+// g++ -Wall -Wextra -std=c++03 -I../include -o to_int.exe to_int.cpp && to_int x1
