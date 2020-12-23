f5bd147be758f6876555ae79fda3f74585b97712
JavaCommons Technologies <javacommons@gmail.com>
2020-12-23 19:05:50 +0900

    .

--- demo/wrap.cpp
+++ demo/wrap.cpp
@@ -3,11 +3,15 @@
 #include <iomanip>
 
 void sub();
-void sub2(unicode_ostream &aout);
+void sub2(unicode_ostream& aout);
 
 int main()
 {
     using namespace std;
+
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
     //unicode_ostream aout(cout);
     unicode_ostream aout(cout, GetConsoleCP());
 
@@ -29,7 +33,7 @@ int main()
     cout << ansi << endl;
     char8 = ansi_to_char8(ansi);
     aout << char8 << endl;
- 
+
     std::string sjis = char8_to_sjis(char8);
     cout << sjis << endl;
     char8 = sjis_to_char8(sjis);
@@ -40,7 +44,7 @@ int main()
     char8 = utf8_to_char8(utf8);
     aout << char8 << endl;
 #endif
- 
+
     //aout << 1 << u8" char*漢字 " << std::string(u8" string漢字 ") << 1.2345 << std::endl;
     aout << 1 << U8(" char*漢字© ") << std::string(U8(" string漢字© ")) << 1.2345 << std::endl;
     aout << 1 << L" wchar_t*漢字© " << std::wstring(L" wstring漢字© ") << 1.2345 << std::endl;
@@ -55,8 +59,8 @@ int main()
 
     // 書式指定(B) setbase(10) の代わりに dec を使うこともできます
     aout << setbase(10) << right << setw(15)
-         << setfill('_') << showpos
-         << fixed << setprecision(2);
+        << setfill('_') << showpos
+        << fixed << setprecision(2);
     // 実際の印字処理(B)
     aout << B << endl;
 
