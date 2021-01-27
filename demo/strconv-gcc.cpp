6175e9756c16baa71a6947fe26a88a563ac19b04
JavaCommons Technologies <javacommons@gmail.com>
2021-01-28 08:07:18 +0900

    .

--- demo/strconv-gcc.cpp
+++ demo/strconv-gcc.cpp
@@ -2,12 +2,18 @@
 #include <iostream>
 #include <string>
 
+void msgbox(const wchar_t *caption, UINT uType=MB_OK, const wchar_t *format=NULL,...)
+{
+    MessageBoxW(NULL, caption, caption, uType);
+}
+
 int main(void)
 {
-    std::string nameUtf8 = u8str(u8"太郎");
+    std::string nameUtf8 = U8("太郎");
     int age = 15;
     std::cout << utf8_to_ansi(format("ハロー、私の名前は %s。 年は %d だ!", nameUtf8.c_str(), age)) << std::endl;
     std::cout << formatA("ハロー、私の名前は %s。 年は %d だ!", nameUtf8.c_str(), age) << std::endl;
+    msgbox(L"msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©");
     std::wstring nameWide = L"花子";
     age = 23;
     std::cout << wide_to_ansi(format(L"ハロー、私の名前は %s。 年は %d だ!", nameWide.c_str(), age)) << std::endl;
