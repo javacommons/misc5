19e3efabee10dbea57790890f8ca7c21cb29a835
JavaCommons Technologies <javacommons@gmail.com>
2021-01-28 11:34:31 +0900

    .

--- demo/strconv-gcc.cpp
+++ demo/strconv-gcc.cpp
@@ -2,9 +2,21 @@
 #include <iostream>
 #include <string>
 
-void msgbox(const wchar_t *caption, UINT uType=MB_OK, const wchar_t *format=NULL,...)
+void msgbox(const std::wstring &caption, UINT uType = MB_OK, const wchar_t *format = NULL, ...)
 {
-    MessageBoxW(NULL, caption, caption, uType);
+    std::wstring s;
+    if (!format)
+    {
+        s = caption;
+    }
+    else
+    {
+        va_list args;
+        va_start(args, format);
+        s = vformat(format, args);
+        va_end(args);
+    }
+    MessageBoxW(NULL, s.c_str(), caption.c_str(), uType);
 }
 
 int main(void)
@@ -14,6 +26,7 @@ int main(void)
     std::cout << utf8_to_ansi(format("ハロー、私の名前は %s。 年は %d だ!", nameUtf8.c_str(), age)) << std::endl;
     std::cout << formatA("ハロー、私の名前は %s。 年は %d だ!", nameUtf8.c_str(), age) << std::endl;
     msgbox(L"msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©msgboxテスト©");
+    msgbox(L"msgboxテスト", MB_OK, L"test=%d", 123);
     std::wstring nameWide = L"花子";
     age = 23;
     std::cout << wide_to_ansi(format(L"ハロー、私の名前は %s。 年は %d だ!", nameWide.c_str(), age)) << std::endl;
