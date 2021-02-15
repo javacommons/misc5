a053cc16c887cbb030d27368c0b013bd5fb8b589
JavaCommons Technologies <javacommons@gmail.com>
2021-02-15 17:16:38 +0900

    .

--- waxeye/run-json.sh
+++ waxeye/run-json.sh
@@ -1,9 +1,12 @@
 #! bash -uvx
 pacman -S --needed --noconfirm \
 	$MINGW_PACKAGE_PREFIX-toolchain \
+    $MINGW_PACKAGE_PREFIX-boost \
 	$MINGW_PACKAGE_PREFIX-gtest \
-	$MINGW_PACKAGE_PREFIX-openssl
+	$MINGW_PACKAGE_PREFIX-openssl \
+    make
 
 waxeye -g c . -p json json.waxeye
-gcc json.c json_parser.c -lwaxeye -o json
+gcc -c json_parser.c
+g++ -std=c++17 json.cpp json_parser.o -lwaxeye -o json
 ./json.exe 
