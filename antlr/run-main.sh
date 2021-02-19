42662c3549eb454bc007d78fb87f7c38e11fa577
JavaCommons Technologies <javacommons@gmail.com>
2021-02-20 07:34:26 +0900

    .

--- antlr/run-main.sh
+++ antlr/run-main.sh
@@ -1,4 +1,3 @@
 #! bash -uvx
-antlr.exe -Dlanguage=Cpp -listener -visitor -o generated/ -package antlrcpptest TLexer.g4 TParser.g4
+antlr-4.9.1.exe -Dlanguage=Cpp -listener -visitor -o generated/ -package antlrcpptest TLexer.g4 TParser.g4
 g++ -I generated main.cpp generated/*.cpp -lantlr4-runtime -o main && main
-
