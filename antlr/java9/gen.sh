8e403c25161604144ca70b91773186309e37feba
JavaCommons Technologies <javacommons@gmail.com>
2021-02-20 10:21:03 +0900

    .

--- antlr/java9/gen.sh
+++ antlr/java9/gen.sh
@@ -1,3 +1,3 @@
 #! bash -uvx
 #antlr-4.9.1.exe -Dlanguage=Java -listener -o src/main/java/antlrtest -package antlrtest Java9.g4
-antlr-4.9.1.exe -Dlanguage=Java -listener -o src/main/java Java9.g4
+antlr-4.9.1.exe -Dlanguage=Java -listener -visitor -o src/main/java Java9.g4
--- antlr/java9/src/main/java/Main.java
+++ antlr/java9/src/main/java/Main.java
@@ -4,7 +4,32 @@ import java.util.List;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.Trees;
+
+class EvalVisitor extends Java9BaseVisitor<Void> {
+
+	Parser parser;
+
+	public EvalVisitor(Parser parser) {
+		this.parser = parser;
+	}
+
+	@Override
+	public Void visitImportDeclaration(Java9Parser.ImportDeclarationContext ctx) {
+		String label = Trees.getNodeText(ctx, this.parser);
+		System.out.printf("visit: %s\n", label);
+		for (int i = 0; i < ctx.children.size(); ++i) {
+			ParseTree c = ctx.children.get(i);
+			System.out.printf("c: %s\n", c.getText());
+		}
+		//visitChildren(ctx);
+		return (Void) null;
+	}
+
+}
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -23,5 +48,6 @@ public class Main {
 		List<String> ruleNamesList = Arrays.asList(parser.getRuleNames());
 		String prettyTree = TreeUtils.toPrettyTree(root, ruleNamesList);
 		System.out.println(prettyTree);
+		new EvalVisitor(parser).visit(root);
 	}
 }
