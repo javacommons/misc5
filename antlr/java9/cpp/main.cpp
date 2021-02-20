99378611812a3d75b2b58a39afe6bb00c7e98bc4
JavaCommons Technologies <javacommons@gmail.com>
2021-02-21 03:13:40 +0900

    .

--- antlr/java9/cpp/main.cpp
+++ antlr/java9/cpp/main.cpp
@@ -9,17 +9,59 @@ using json = nlohmann::json;
 #include <string>
 #include <sstream>
 #include <regex>
-#include <stack>
+//#include <stack>
+#include <deque>
 
 #include "antlr4-runtime.h"
 #include "Java8Lexer.h"
 //#include "Java8Parser.h"
-#include "Java8ParserBaseVisitor.h"
+//#include "Java8ParserBaseVisitor.h"
+#include "Java8ParserBaseListener.h"
 
 #include "strconv.h"
 
 using namespace antlrcpptest;
 
+class MyListener : public Java8ParserBaseListener
+{
+    antlr4::Parser *parser;
+    jcommons::antlr4::TreeUtils *tu;
+    std::deque<json> result_stack;
+public:
+    MyListener(antlr4::Parser *parser): parser(parser)
+    {
+        this->tu = new jcommons::antlr4::TreeUtils(parser);
+        json top = json::array();
+        result_stack.push_front(top);
+    }
+    MyListener(antlr4::Parser &parser): MyListener(&parser) {}
+    ~MyListener() { result_stack.pop_front(); }
+
+    // Java8ParserListener interface
+public:
+    json getJsonResult() {
+        json &top = result_stack[0];
+        return top;
+    }
+
+    virtual void enterImportDeclaration(Java8Parser::ImportDeclarationContext *ctx)
+    {
+        json self;
+        self["rule"] = "importDeclaration";
+        self["text"] = tu->getTextList(ctx);
+        result_stack.push_front(self);
+    }
+
+    virtual void exitImportDeclaration(Java8Parser::ImportDeclarationContext *ctx)
+    {
+        json &self = result_stack[0];
+        json &parent = result_stack[1];
+        parent.push_back(self);
+        result_stack.pop_front();
+    }
+};
+
+#if 0x0
 class MyVisitor : public Java8ParserBaseVisitor
 {
     antlr4::Parser *parser;
@@ -57,6 +99,7 @@ public:
     }
 
 };
+#endif
 
 int main()
 {
@@ -82,9 +125,16 @@ int main()
     jcommons::antlr4::TreeUtils tu(parser);
     std::string pretty = tu.toPrettyTree(tree);
     uout << "Pretty: " << pretty << std::endl;
+#if 0x0
     MyVisitor v(parser);
     antlrcpp::Any result = v.visit(tree);
     uout << GetConsoleCP() << result.as<json>().dump(2) << std::endl;
+#endif
+
+    MyListener listener(parser);
+    antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+    json l_result = listener.getJsonResult();
+    uout << l_result.dump(2) << std::endl;
 
     return 0;
 }
