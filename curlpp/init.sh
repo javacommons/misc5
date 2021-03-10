f1f43068b78379b20e2990dbed755813bc15c680
JavaCommons Technologies <javacommons@gmail.com>
2021-03-10 17:32:03 +0900

    .

---
+++ curlpp/init.sh
@@ -0,0 +1,22 @@
+#! bash -uvx
+pacman -S --needed --noconfirm \
+	$MINGW_PACKAGE_PREFIX-toolchain \
+	$MINGW_PACKAGE_PREFIX-cmake \
+    make \
+    subversion \
+    unzip \
+    wget
+
+cwd=`pwd`
+
+if [ ! -e curlpp ]; then
+  svn export https://github.com/jpbarrette/curlpp/trunk curlpp
+  rm -rf build-curlpp
+  mkdir -p build-curlpp
+  cd build-curlpp
+  cmake -G "MinGW Makefiles" ../curlpp
+  cmake --build . --config Release
+  cp -rp ../curlpp/include/* $MINGW_PREFIX/include/
+  cp -rp *.a $MINGW_PREFIX/lib/
+  cp -rp *.dll $MINGW_PREFIX/bin/
+fi
