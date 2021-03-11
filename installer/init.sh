907f7d96bbb8deed284887e7d08801d10c2229c7
JavaCommons Technologies <javacommons@gmail.com>
2021-03-11 14:53:25 +0900

    .

--- installer/init.sh
+++ installer/init.sh
@@ -4,13 +4,30 @@ pacman -S --needed --noconfirm \
 	$MINGW_PACKAGE_PREFIX-cmake \
 	$MINGW_PACKAGE_PREFIX-nim \
 	$MINGW_PACKAGE_PREFIX-nimble \
+	$MINGW_PACKAGE_PREFIX-boost \
+	$MINGW_PACKAGE_PREFIX-wxWidgets \
     make \
     unzip \
-	subversin \
+	subversion \
     wget
+
+cwd=`pwd`
 
 # https://github.com/sinkingsugar/nimline
 nimble install nimline -y
 
 rm -rf NiGui
 svn export https://github.com/trustable-code/NiGui/trunk NiGui
+
+rm -rf boost.ui
+#svn export https://github.com/kosenko/ui/trunk boost.ui
+
+#cd boost.ui/build
+
+#export WX_CONFIG=mingw32/bin/wx-config
+#export WXWIN=$MINGW_PREFIX
+#export BOOST_ROOT=$MINGW_PREFIX
+#b2 libs/ui/build/ link=static
+#b2 libs/ui/test/ link=static
+#b2 libs/ui/example/ link=static
+#b2 libs/ui/example/cpp11/ link=static
---
+++ installer/wx01.sh
@@ -0,0 +1,2 @@
+#! bash -uv
+g++ -I /mingw32/include/wx-3.0 -I /mingw32/lib/wx/include/msw-unicode-3.0 wx01.cpp
