daa4cee27497476471f045f0328672bf3d12e3be
JavaCommons Technologies <javacommons@gmail.com>
2021-03-12 13:37:41 +0900

    .

--- sqlite/init.sh
+++ sqlite/init.sh
@@ -3,6 +3,7 @@ pacman -S --needed --noconfirm \
 	$MINGW_PACKAGE_PREFIX-gcc \
 	$MINGW_PACKAGE_PREFIX-cmake \
 	$MINGW_PACKAGE_PREFIX-boost \
+	$MINGW_PACKAGE_PREFIX-sqlite3 \
     git \
     make \
 	subversion \
@@ -15,5 +16,7 @@ rm -rf sqlite_orm
 svn export https://github.com/fnc12/sqlite_orm/trunk sqlite_orm
 rm -rf $MINGW_PREFIX/include/sqlite_orm
 cp -rp sqlite_orm/include/sqlite_orm $MINGW_PREFIX/include/
+rm -rf examples
+cp -rp sqlite_orm/examples .
 rm -rf tests
 cp -rp sqlite_orm/tests .
